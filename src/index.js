5286242cfc64daf27b987f6c9d262981053fe284
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-02-15 16:58:41 +0100

    babystep 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const animalsToAdopt = [
+  {
+      name: "Lucky",
+      picture: "https://placekitten.com/200/287",
+  },
+  {
+      name: "Symba",
+      picture: "https://placekitten.com/200/139",
+  },
+  {
+      name: "Léo",
+      picture: "https://placekitten.com/200/90",
+  },
+  {
+      name: "Milo",
+      picture: "https://placekitten.com/200/194",
+  },
+  {
+      name: "Charly",
+      picture: "https://placekitten.com/200/179",
+  },
+];
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  React.createElement("h1", {}, "Hello React!")
+  [
+    <div>{ animalsToAdopt[0].name }</div>,
+    <div>animalsToAdopt[1].name</div>,
+    <div>animalsToAdopt[2].name</div>,
+    <div>animalsToAdopt[3].name</div>,
+    <div>animalsToAdopt[4].name</div>,
+  ]
 );
